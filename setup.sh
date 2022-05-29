dccd595dc23b093d79a225893a2a6bc8015fcd80
kamini-singh <kaminisingh540@gmail.com>
2022-05-29 05:36:27 +0530

    changes in config.toml

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@ echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-
-echo "\
+echo "[theme]
+pbase='light'
+primaryColor='#4d4c50'
+backgroundColor='#fdf8f8'
+textColor='#0a0000'
+font = ‘sans serif’
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
