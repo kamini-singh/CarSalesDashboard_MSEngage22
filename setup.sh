063cb0c069abef610a470aba8f9c196bfdc2400a
kamini-singh <kaminisingh540@gmail.com>
2022-05-29 05:42:54 +0530

    changes made for deployment

--- setup.sh
+++ setup.sh
@@ -4,14 +4,9 @@ echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "[theme]
-pbase='light'
-primaryColor='#4d4c50'
-backgroundColor='#fdf8f8'
-textColor='#0a0000'
-font = ‘sans serif’
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" >> ~/.streamlit/config.toml
